3609ff3ce9560982bcfce5f43850af09ad9e4bc5
Kaml Romodan <Kaml.Romodan@gmail.com>
2021-11-06 13:33:33 +0300

    Done with Part 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from "@angular/http";
-import { map } from 'rxjs/add/operator/map';
+import { Http } from "@angular/http";
+import { map } from 'rxjs';
 
 @Injectable()
 export class ProductService {
-private _albumUrl: string = '../assets/album.json'
-  constructor(private _http: Http) { }
+  private _albumUrl: string = '../assets/album.json';
 
-  getAlbum(id:number) {
-    return this._http.get(this._albumUrl).map((response) => {
-      response.json();
-    })
+  constructor(private _http: Http) { };
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json()
+    )
   }
 }
